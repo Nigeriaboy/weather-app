2aeb155497396bcdc409239b985fff1f4ad550ac
Nigeriaboy <abdurahmonabdullah247@gmail.com>
2026-06-26 23:04:43 +0100

    CREATION OF FILES
    Created all the needed files and tested the API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+import { sub } from "date-fns";
 import "./styles.css";
+
+const locationInput = document.querySelector("#location");
+const submitBtn = document.querySelector("#submitBtn");
+
+async function getWeather(location) {
+  if (location) {
+    const data = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=Y63Y38UT9H68LNE4LBRUFYHK3`,
+    );
+
+    const dataJson = await data.json();
+
+    return dataJson;
+  } else {
+    return 1;
+  }
+}
+
+submitBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  if (locationInput.value) {
+    const weather = await getWeather(locationInput.value);
+
+    console.log(weather.description);
+  }
+});
